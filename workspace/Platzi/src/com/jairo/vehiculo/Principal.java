e16fcf58109419e3415bcc6373fbea1a9125a6c3
jairo <jairo.cab17@gmail.com>
2017-07-26 01:12:53 -0300

    Se actualiza contenido

--- workspace/Platzi/src/com/jairo/vehiculo/Principal.java
+++ workspace/Platzi/src/com/jairo/vehiculo/Principal.java
@@ -1,12 +1,10 @@
 package com.jairo.vehiculo;
 
-import java.sql.SQLException;
-//import java.util.ArrayList;
-import com.jairo.basededatos.BaseDeDatos;
+import java.util.ArrayList;
 
 public class Principal {
 
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) {
 		
 		//Tipo de dato nombreobjt
 		
@@ -18,24 +16,31 @@ public class Principal {
 		miVehiculo.arrancar();
 		*/
 		
-//		ArrayList<Vehiculo> vehiculos = new ArrayList<>();	
-//		vehiculos.add(miVehiculo);
-//		vehiculos.add(new Vehiculo("BBB123", "Chevrolet"));
-//		vehiculos.add(new Vehiculo("CCC123", "Mercedez Benz", "s10", 2014));
+//		ArrayList<Integer> miLista = new ArrayList<Integer>();
+//		miLista.add(1);
+//		miLista.add(2);
+//		miLista.add(3);
 //		
-//		for (Vehiculo vehiculo : vehiculos) {
-//			
-//			System.out.println(vehiculo.getMatricula());
-//			System.out.println(vehiculo.getMarca());
-//			System.out.println(vehiculo.getModelo());
-//			System.out.println(vehiculo.getAnio());
-//			
-//			System.out.println("------------------");
-//			
-//		}
+//		for(Integer i: miLista)
+//		{
+//			System.out.println(i);
+//		}		
 		
-		BaseDeDatos baseDatos = new BaseDeDatos();
-		baseDatos.conectar();
+		ArrayList<Vehiculo> vehiculos = new ArrayList<>();	
+		vehiculos.add(miVehiculo);
+		vehiculos.add(new Vehiculo("BBB123", "Chevrolet"));
+		vehiculos.add(new Vehiculo("CCC123", "Mercedez Benz", "s10", 2014));
+		
+		for (Vehiculo vehiculo : vehiculos) {
+			
+			System.out.println(vehiculo.getMatricula());
+			System.out.println(vehiculo.getMarca());
+			System.out.println(vehiculo.getModelo());
+			System.out.println(vehiculo.getAnio());
+			
+			System.out.println("------------------");
+			
+		}
 		
 //		Taxi taxi = new Taxi("AAA123", "Hiundai", "i10",1990);
 //		taxi.setAnio(2000);
